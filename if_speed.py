e6ae325c450d3495a75e3a58aa193fea043bff25
VanDerMelk <melik-me@protonmail.ch>
2018-04-01 19:21:14 +0300

    Task about speed with if

---
+++ if_speed.py
@@ -0,0 +1,9 @@
+#if-else construction
+speed = input("Enter speed: ")
+print(type(speed))
+speed = int(speed)
+if speed>=80:
+    print("You so busted!")
+else:
+    print("Have a nice day!")
+print(type(speed))
